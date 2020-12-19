3ce602b5ec251c36d2c8c007ef4232fcb8d67026
Sidwrite <40756494+Sidwrite@users.noreply.github.com>
2020-12-19 15:38:25 +0300

    Create 3.sql

---
+++ Lesson_09/3.sql
@@ -0,0 +1,51 @@
+-- 1. Создайте хранимую функцию hello(), которая будет возвращать приветствие, в зависимости от текущего времени суток. С 6:00 до 12:00 функция должна возвращать фразу "Доброе утро", с 12:00 до 18:00 функция должна возвращать фразу "Добрый день", с 18:00 до 00:00 — "Добрый вечер", с 00:00 до 6:00 — "Доброй ночи".
+
+DROP PROCEDURE IF EXISTS hello;
+delimiter //
+CREATE PROCEDURE hello()
+BEGIN
+	IF(CURTIME() BETWEEN '06:00:00' AND '12:00:00') THEN
+		SELECT 'Доброе утро';
+	ELSEIF(CURTIME() BETWEEN '12:00:00' AND '18:00:00') THEN
+		SELECT 'Добрый день';
+	ELSEIF(CURTIME() BETWEEN '18:00:00' AND '00:00:00') THEN
+		SELECT 'Добрый вечер';
+	ELSE
+		SELECT 'Доброй ночи';
+	END IF;
+END //
+delimiter ;
+
+CALL hello();
+
+-- 2. 	(В таблице products есть два текстовых поля: name с названием товара и description с его описанием. Допустимо присутствие обоих полей или одно из них. Ситуация, когда оба поля принимают неопределенное значение NULL неприемлема. Используя триггеры, добейтесь того, чтобы одно из этих полей или оба поля были заполнены. При попытке присвоить полям NULL-значение необходимо отменить операцию.
+
+DROP TRIGGER IF EXISTS trigger_null;
+delimiter //
+CREATE TRIGGER trigger_null BEFORE INSERT ON products
+FOR EACH ROW
+BEGIN
+	IF(ISNULL(NEW.name) AND ISNULL(NEW.description)) THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Both fields are empty!';
+	END IF;
+END //
+delimiter ;
+
+-- 3. (по желанию) Напишите хранимую функцию для вычисления произвольного числа Фибоначчи. Числами Фибоначчи называется последовательность в которой число равно сумме двух предыдущих чисел. Вызов функции FIBONACCI(10) должен возвращать число 55.
+
+
+-- Не успел по сроку доделать:-( Запутался чутка, на JS несколько вариантов работает))
+
+DROP FUNCTION IF EXISTS fib;
+delimiter //
+CREATE FUNCTION fib (n INT)
+RETURNS INT DETERMINISTIC
+BEGIN
+--    if (n <= 1 ) {
+--        return 1;
+--    } else {
+--        return fibo(n - 1) + fibo(n - 2);
+ END //
+delimiter ;
+
+SELECT fib(1);
